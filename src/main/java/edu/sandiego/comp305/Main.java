7ea8144374f9953fe1b8fea7e9193dee1e951a5b
Austin Lorja <austinlorja@sandiego.edu>
2026-09-17 10:03:25 -0700

    added another greeting

--- src/main/java/edu/sandiego/comp305/Main.java
+++ src/main/java/edu/sandiego/comp305/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Dr. Krause-Levy!");
         System.out.println("Hello, Austin");
+        System.out.println("Hello, chicken");
         System.out.println("Welcome to COMP305!");
     }
 }
