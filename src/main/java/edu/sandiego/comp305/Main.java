ce0a015664f959d422f74ed15da2002f10d7ec76
Oriana Castillo <Ocastillo@sandiego.edu>
2026-09-17 10:03:08 -0700

    Add greeting for Oriana

--- src/main/java/edu/sandiego/comp305/Main.java
+++ src/main/java/edu/sandiego/comp305/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Dr. Krause-Levy!");
         System.out.println("Welcome to COMP305!");
+        System.out.println("Hello, Oriana");
+
     }
 }
