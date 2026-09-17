d966e8c5484facee89fd2ce302b912654300ceec
Matios Zeleke <mzeleke@sandiego.edu>
2026-09-17 10:02:14 -0700

    Added 3rd Print statement in main.java

--- src/main/java/edu/sandiego/comp305/Main.java
+++ src/main/java/edu/sandiego/comp305/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Dr. Krause-Levy!, Hello Matios Zeleke");
         System.out.println("Welcome to COMP305!");
+        System.out.println("Hello, Emily");
     }
 }
