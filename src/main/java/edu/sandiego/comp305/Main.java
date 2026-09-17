eaa64e51cdbcb8aef2e4cb43ae899c99602489f3
Brayden Zarefsky <bzarefsky@sandiego.edu>
2026-09-17 10:17:00 -0700

    feat - Added John to Main.java

--- src/main/java/edu/sandiego/comp305/Main.java
+++ src/main/java/edu/sandiego/comp305/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Dr. Krause-Levy!");
         System.out.println("Hello, Brayden");
+        System.out.println("Hello John");
         System.out.println("Welcome to COMP305!");
     }
 }
