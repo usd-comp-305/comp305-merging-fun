70cc2f77b5597eae8d3ae5cbc9c988d015d41476
Jack Conley <128567631+jacco44@users.noreply.github.com>
2026-09-17 10:25:15 -0700

    Added name print statement in main print statement

--- src/main/java/edu/sandiego/comp305/Main.java
+++ src/main/java/edu/sandiego/comp305/Main.java
@@ -1,8 +1,11 @@
 package edu.sandiego.comp305;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Dr. Krause-Levy!");
+        System.out.println("Hello, Jack");
+        System.out.println("Hello, Peter Parker");
         System.out.println("Welcome to COMP305!");
     }
 }
