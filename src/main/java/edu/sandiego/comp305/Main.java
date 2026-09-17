8e4a1313ae9bf18187cffe2247e8296f699ae3ee
Matios Zeleke <mzeleke@sandiego.edu>
2026-09-17 10:08:28 -0700

    3rdNameAdded

--- src/main/java/edu/sandiego/comp305/Main.java
+++ src/main/java/edu/sandiego/comp305/Main.java
@@ -2,7 +2,8 @@ package edu.sandiego.comp305;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Dr. Krause-Levy!, Hello Matios Zeleke");
+        System.out.println("Hello, Dr. Krause-Levy");
+        System.out.println("Hello Matios Zeleke");
         System.out.println("Welcome to COMP305!");
         System.out.println("Hello, Emily");
     }
